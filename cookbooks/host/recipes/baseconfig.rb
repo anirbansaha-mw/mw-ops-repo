c9e7e3049a991c0509accca8334b42b4da379cc2
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-19 12:40:10 +0800

    refer modifying cookbook host

--- cookbooks/host/recipes/baseconfig.rb
+++ cookbooks/host/recipes/baseconfig.rb
@@ -12,3 +12,17 @@ userdata = open('http://169.254.169.254/latest/user-data/').read
 pre_hostname, pre_tier = "#{userdata}".split
 hostname = "#{pre_hostname}".split("=")[1]
 tier = "#{pre_tier}".split("=")[1]
+
+template "/etc/hostname" do
+  source "hostname.erb"
+  variables({
+    :host_name => "#{hostname}"
+  })
+  notifies :run, "execute[set-hostname]"
+end
+
+execute "set-hostname" do
+  command "/sbin/start hostname"
+  action :nothing
+  subscribes :run, "template[/etc/hostname]", :immediately
+end
