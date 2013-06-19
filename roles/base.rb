e31da1e7bc4784026c1278de52d3925a495e4b71
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-19 15:05:32 +0800

    mod

--- roles/base.rb
+++ roles/base.rb
@@ -2,5 +2,6 @@ name "base"
 description "Base role (generic configuration)"
 run_list(
        "recipe[host::baseconfig]",
-       "recipe[host::ntp]"
+       "recipe[host::ntp]",
+       "recipe[host::users]"
 )
