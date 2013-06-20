08e510780f9e06cb836b32adcd96b23ea79c8b13
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-20 17:04:17 +0800

    adding daemontools

---
+++ cookbooks/host/recipes/daemontools.rb
@@ -0,0 +1,56 @@
+#
+# Cookbook Name:: host
+# Recipe:: daemontools
+# Author : Anirban Saha (anirban.saha@mobilewalla.com)
+# Copyright 2013, Mobilewalla Pte. Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "daemontools" do
+  action [:install]
+end
+
+package "daemontools-run" do
+  action [:install]
+  notifies :run, "execute[start-daemontools]", :immediately
+end
+
+execute "start-daemontools" do
+  command "/sbin/initctl start svscan"
+  action :nothing
+  subscribes :run, "package[daemontools-run]", :immediately
+end
+
+link "/service" do
+  to "/etc/service"
+end
+
+directory "/service/chef-client-sync" do
+    mode        "0755"
+    owner       "root"
+    group       "root"
+    action      :create
+end
+
+directory "/service/chef-client-sync/log" do
+    mode        "0755"
+    owner       "root"
+    group       "root"
+    action      :create
+end
+
+template "/service/chef-client-sync/log/run" do
+  source "daemontools_log_run.erb"
+end
+
+template "/service/chef-client-sync/run" do
+  source "daemontools_run.erb"
+  notifies :run, "execute[start-sync-service]", :immediately
+end
+
+execute "start-sync-service" do
+  command "/usr/bin/svc -t /service/chef-client-sync"
+  action :nothing
+  subscribes :run, "template[/service/chef-client-sync/run]", :immediately
+end
--- roles/base.rb
+++ roles/base.rb
@@ -3,5 +3,6 @@ description "Base role (generic configuration)"
 run_list(
        "recipe[host::baseconfig]",
        "recipe[host::ntp]",
-       "recipe[host::users]"
+       "recipe[host::users]",
+       "recipe[host::daemontools]"
 )
