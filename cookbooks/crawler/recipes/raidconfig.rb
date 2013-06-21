d3a130b858bf830905168adf148c4579d445d54c
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-21 22:01:21 +0800

    add cookbook crawler

---
+++ cookbooks/crawler/recipes/raidconfig.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: crawler
+# Recipe:: raidconfig
+# Author : Anirban Saha (anirban.saha@mobilewalla.com)
+# Copyright 2013, Mobilewalla Pte. Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "mdadm" do
+  action [:install]
+end
+
+template "/etc/ntp.conf" do
+  source "ntp.conf.erb"
+  notifies :restart, "service[ntp]", :immediately
+end
+
+service "ntp" do
+  action [:enable, :start]
+  subscribes :reload, "template[/etc/ntp.conf]", :immediately
+end
