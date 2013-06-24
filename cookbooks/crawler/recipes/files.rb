33a6f29027b8a68e852d8c4c7190b0a76016de8d
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-24 11:04:22 +0800

    mod

---
+++ cookbooks/crawler/recipes/files.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: crawler
+# Recipe:: files
+# Author : Anirban Saha (anirban.saha@mobilewalla.com)
+# Copyright 2013, Mobilewalla Pte. Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{/mnt/local/wordster /mnt/local/wordster/mw  /mnt/local/wordster/mw/crawler /mnt/local/wordster/mw/crawler/apple}.each do |dir|
+
+directory "#{dir}" do
+    mode        "0755"
+    owner       "wordster"
+    group       "wordster"
+    action      :create
+end
