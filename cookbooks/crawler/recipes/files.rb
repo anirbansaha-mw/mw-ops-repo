c16db3dba3024b43d93d9392b6a59d11b1000653
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-24 11:25:25 +0800

    mod

--- cookbooks/crawler/recipes/files.rb
+++ cookbooks/crawler/recipes/files.rb
@@ -7,11 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w{/mnt/local/wordster /mnt/local/wordster/mw  /mnt/local/wordster/mw/crawler /mnt/local/wordster/mw/crawler/apple}.each do |dir|
-
+%w{/mnt/local/wordster /mnt/local/wordster/mw  /mnt/local/wordster/mw/crawler /mnt/local/wordster/mw/crawler/apple /mnt/local/wordster/mw/crawler/apple/rank_rt}.each do |dir|
 directory "#{dir}" do
     mode        "0755"
     owner       "wordster"
     group       "wordster"
     action      :create
+ end
 end
