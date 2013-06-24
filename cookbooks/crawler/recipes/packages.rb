44366b89101a5d1cfa4955e13ce334fafcf03c0b
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-24 15:29:22 +0800

    mod

---
+++ cookbooks/crawler/recipes/packages.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: host
+# Recipe:: packages
+# Author : Anirban Saha (anirban.saha@mobilewalla.com)
+# Copyright 2013, Mobilewalla Pte. Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "openjdk-6-jre-headless" do
+  action [:install]
+end
