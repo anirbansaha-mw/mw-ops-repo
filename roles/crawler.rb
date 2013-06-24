8dfbaca777e55ab7a1fa390510eba2ccc64102d6
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-24 11:05:10 +0800

    mod

--- roles/crawler.rb
+++ roles/crawler.rb
@@ -5,5 +5,6 @@ run_list(
        "recipe[host::ntp]",
        "recipe[host::users]",
        "recipe[host::daemontools]",
-       "recipe[crawler::raidconfig]"
+       "recipe[crawler::raidconfig]",
+       "recipe[crawler::files]"
 )
