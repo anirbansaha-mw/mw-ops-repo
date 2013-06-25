f0fb1783401e7a928fd632d53cd212d4b8ccff86
Anirban Saha <anirban.saha@mobilewalla.com>
2013-06-26 01:36:12 +0800

    mod

--- cookbooks/crawler/recipes/cron.rb
+++ cookbooks/crawler/recipes/cron.rb
@@ -7,92 +7,30 @@
 # All rights reserved - Do Not Redistribute
 #
 
-cron "instance1_cron" do
+%w{1 3 5 7}.each do |num1|
+cron "instance#{num1}_cron" do
   minute "0"
   hour "*"
   day "*"
   month "*"
   weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance1/itunesRank.sh"
+  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance#{num1}/itunesRank.sh"
   user "wordster"
   action :create
+ end
 end
 
-cron "instance2_cron" do
+%w{2 4 6 8}.each do |num2|
+cron "instance#{num2}_cron" do
   minute "30"
   hour "*"
   day "*"
   month "*"
   weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance2/itunesRank.sh"
+  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance#{num2}/itunesRank.sh"
   user "wordster"
   action :create
-end
-
-cron "instance3_cron" do
-  minute "0"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance3/itunesRank.sh"
-  user "wordster"
-  action :create
-end
-
-cron "instance4_cron" do
-  minute "30"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance4/itunesRank.sh"
-  user "wordster"
-  action :create
-end
-
-cron "instance5_cron" do
-  minute "0"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance5/itunesRank.sh"
-  user "wordster"
-  action :create
-end
-
-cron "instance6_cron" do
-  minute "30"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance6/itunesRank.sh"
-  user "wordster"
-  action :create
-end
-
-cron "instance7_cron" do
-  minute "0"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance7/itunesRank.sh"
-  user "wordster"
-  action :create
-end
-
-cron "instance8_cron" do
-  minute "30"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "/mnt/local/wordster/mw/crawler/apple/rank_rt/instance8/itunesRank.sh"
-  user "wordster"
-  action :create
+ end
 end
 
 cron "nagios1_cron" do
